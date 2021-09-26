130838d14646719b74e1c96c4ff986421fc9c7cf
FirinKinuo <deals@fkinuo.ru>
2021-09-26 12:49:04 +0300

    UPDATE: Add pathlib support for watchdog, watching for archives, change __main__.py structure, change env key

--- app/__main__.py
+++ app/__main__.py
@@ -1,25 +1,24 @@
 from ast import literal_eval
 from os import environ
-from app.cam_watchdog import CamWatchdog
+from app.filesystem import Watchdog
 from app.web.api import Api
 from asyncio import get_event_loop
 
 
-CAM_WATCHDOG = CamWatchdog(literal_eval(environ.get('CAM_SERVER_LIST')))
-API_SERVER = Api(host=environ.get('API_SERVER_HOST'), port=int(environ.get('API_SERVER_PORT')))
-
-
 def run() -> None:
-    """Запустить проект"""
-    async_loop = get_event_loop()
+    """Запуск проекта"""
+    filesystem_watchdog = Watchdog(literal_eval(environ.get('ARCHIVES_LIST')))
+    api_server = Api(host=environ.get('API_SERVER_HOST'), port=int(environ.get('API_SERVER_PORT')))
 
-    async_loop.create_task(API_SERVER.start())
-    async_loop.create_task(CAM_WATCHDOG.start())
+    filesystem_watchdog.start()
+
+    async_loop = get_event_loop()
+    async_loop.create_task(api_server.start())
 
     try:
         async_loop.run_forever()
     finally:
-        for runner in API_SERVER.host_runners:
+        for runner in api_server.host_runners:
             async_loop.run_until_complete(runner.cleanup())
 
 
