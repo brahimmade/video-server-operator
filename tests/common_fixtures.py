76090ddaddd0389153d35756b8b2dd6c6120d192
FirinKinuo <deals@fkinuo.ru>
2021-09-29 17:01:43 +0300

    UPDATE:Restructuring tests dir

--- tests/common_fixtures.py
+++ tests/common_fixtures.py
@@ -1,11 +1,22 @@
 import pytest
 import asyncio
 
+from app.database import video_server, BASE
+
 __all__ = [
-    'async_loop'
+    'async_loop',
+    'preload_database'
 ]
 
 
 @pytest.fixture(scope='session')
 def async_loop():
     return asyncio.get_event_loop()
+
+
+@pytest.fixture(scope="session")
+def preload_database():
+    """Предзагрузка базы данных, очистка, инициализация таблиц"""
+    BASE.metadata.drop_all()
+    video_server.init_tables()
+    yield
