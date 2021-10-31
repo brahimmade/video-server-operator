23537ec66405f028311e11da4814d8d5576c2207
FirinKinuo <deals@fkinuo.ru>
2021-10-31 14:03:24 +0300

    FIX: Attribute in get_video

--- tests/integration-tests/test_database_links.py
+++ tests/integration-tests/test_database_links.py
@@ -9,14 +9,14 @@ from app.database import video_server
 
 def test_link_full_path():
     test_dir_path = [path_ for path_ in sys.path if path_.endswith('integration-tests')][0]
-    payload_path = pathlib.Path(test_dir_path, 'test-files/cam-1/2021-05-05/123123/test_video.mp4')
+    payload_path = pathlib.Path(test_dir_path, 'test-files/cam-1313/2021-05-05/123123/test_video.mp4')
     split_path = path.split_path(payload_path)
 
     database_links.set_full_path(video_path=payload_path)
 
     assert video_server.get_server(server_dir=split_path.get('server')) is not None
     assert video_server.get_camera(camera_dir=split_path.get('camera')) is not None
-    assert video_server.get_video(name=split_path.get('video').replace('.mp4', '')) is not None
+    assert video_server.get_video(video_dir=split_path.get('video_path').replace('.mp4', '')) is not None
 
 
 @pytest.mark.parametrize('invalid_path', [
